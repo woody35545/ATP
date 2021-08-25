ff5b9b3de5a0b1aee06371dd8d1e74aeb696d399
KuGunmo <kgm9179@naver.com>
2021-08-25 21:08:27 +0900

    [>] backtasking.py, add comment

--- backtasking.py
+++ backtasking.py
@@ -1,6 +1,6 @@
 import random as r
 import time as t
-
+my_tickers = [""]*10
 class ticker:
     def __init__(self,_name):
         self.name = _name
@@ -18,3 +18,7 @@ while True:
     KRW_XRP.update_current_price()
     print(KRW_XRP.current_price)
     t.sleep(1)
+
+
+# calculator import 해서 실제 상황과 비슷하게 환경 구성후 테스팅 진행, 이후에 안정화 되면 실제 access_key,secret_key를 가지고 api function을 이용하여 실제로 적용
+
